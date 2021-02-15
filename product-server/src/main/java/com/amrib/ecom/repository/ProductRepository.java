15b1e1a9d63dccc0d98c999e0798facf98f0a61f
Bilal AMRI <iambilalamri@gmail.com>
2021-02-15 23:55:53 +0100

    [master]- implement microservice

--- product-server/src/main/java/com/amrib/ecom/repository/ProductRepository.java
+++ product-server/src/main/java/com/amrib/ecom/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.amrib.ecom.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.amrib.ecom.model.Product;
 
+@RepositoryRestResource(collectionResourceRel = "", path = "")
 public interface ProductRepository extends MongoRepository<Product, String> {
 
 }
