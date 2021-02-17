991a439fe0c04cb706bbde8b1253f66dbfb2a2d4
Bilal AMRI <iambilalamri@gmail.com>
2021-02-17 23:41:13 +0100

    [master]- implement gateway's microservice

--- client-service/src/main/java/com/amrib/clientservice/CustomerController.java
+++ client-service/src/main/java/com/amrib/clientservice/CustomerController.java
@@ -1,8 +1,14 @@
 package com.amrib.clientservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
+
+	@GetMapping(value = "/hi/hello")
+	public String hello() {
+		return "Hello World";
+	}
 
 }
