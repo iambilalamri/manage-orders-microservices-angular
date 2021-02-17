30f610aa634e023d52fe1d1ac6a570cd87d9eb36
Bilal AMRI <iambilalamri@gmail.com>
2021-02-17 15:06:02 +0100

    [master]- implement stock's microservice

---
+++ stock-service/src/main/java/com/amrib/stockservice/ProductController.java
@@ -0,0 +1,8 @@
+package com.amrib.stockservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+}
--- stock-service/src/main/java/com/amrib/stockservice/StockServiceApplication.java
+++ stock-service/src/main/java/com/amrib/stockservice/StockServiceApplication.java
@@ -1,13 +1,29 @@
 package com.amrib.stockservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class StockServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(ProductRepository proRepository, RepositoryRestConfiguration configuration) {
+		return args -> {
+			configuration.exposeIdsFor(Product.class);
+			proRepository.save(new Product(null, "produit1", "description1", 125));
+			proRepository.save(new Product(null, "produit2", "description2", 95));
+			proRepository.save(new Product(null, "produit3", "description3", 16));
+			proRepository.save(new Product(null, "produit4", "description4", 147));
+			proRepository.findAll().forEach(System.out::println);
+
+		};
 	}
 
 }
