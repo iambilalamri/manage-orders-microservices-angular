16731143018ad8717332540c6420dba9a459e2a9
Bilal AMRI <iambilalamri@gmail.com>
2021-02-18 12:03:56 +0100

    [master]- implement gateway's microservice

--- gateway-service/src/main/java/com/amrib/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/amrib/gateway/GatewayServiceApplication.java
@@ -4,16 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
-
-	@Bean
-	public ServerCodecConfigurer serverCodecConfigurer() {
-		return ServerCodecConfigurer.create();
 	}
 }
