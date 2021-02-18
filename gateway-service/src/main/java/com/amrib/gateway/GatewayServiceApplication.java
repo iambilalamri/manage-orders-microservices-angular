3a55613da87c80d0b0ea354690b7e4a9c25d20c9
Bilal AMRI <iambilalamri@gmail.com>
2021-02-18 16:11:58 +0100

    [master]- add discovery service and connect all microservice to it

--- gateway-service/src/main/java/com/amrib/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/amrib/gateway/GatewayServiceApplication.java
@@ -2,6 +2,11 @@ package com.amrib.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -11,5 +16,18 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator staticRoute(RouteLocatorBuilder locaBuilder) {
+		// lb: load balancer
+		return locaBuilder.routes().route("r1", r -> r.path("/customers/**").uri("lb://CLIENT-SERVICE"))
+				.route("r2", r -> r.path("/products/**").uri("lb://STOCK-SERVICE")).build();
+	}
+
+	// @Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoute(ReactiveDiscoveryClient discoveryClient,
+			DiscoveryLocatorProperties properties) {
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
 	}
 }
